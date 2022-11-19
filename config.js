be179fb9b1f52c03f70add45add58b4ce3f67a88
slsoformvn <85289441+slsoformvn@users.noreply.github.com>
2022-11-19 14:13:07 +0700

    Add files via upload
    Signed-off-by: slsoformvn <85289441+slsoformvn@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'http://hubt.edu.vn/';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng Cô Phượng';
+const contentCard = ' Nhân ngày 20/11,em xin chúc các thầy các cô lời chúc tốt đẹp nhất. Chúc các thầy cô hoàn thành nhiệm vụ cao cả của mình trong sự nghiệp trăm năm trồng người, trong công cuộc đổi mới của đất nước. Em chân thành cám ơn các thầy cô.';
+
+const giftImage = 'https://lh3.googleusercontent.com/pw/AL9nZEXaaSBxzSn9uau8rAlQvHloNDXaK4xDU00rKakX2XUGXpPj4lJ2eFa443qtYjFTncByTi6BHMH7N3aEnOQpqYHIfY7Ss8xbE1mUFEEjL7DcxdJmNKK-Hyy15cGc9JxwGOPI95G3eCtRjZ-t7YMHbjwoq34V5tdDpNLMfDM5P2m8N8T4X3VQ9Te1S1ZT7M22_RswjGX8FOPLuRxqQ5n99KxfP9IeEkK-XVHWFAmK-FNqc-6qUIe_nSROVhlPW_R8T2gRgCTOmvq0cSDjX8Lv7I-pXQNDkmmSc9mBh-pFqBhIjCwvJCKrXMuPO2_DDLVRUkQV2G9q32YWe5XJw-hLj8FxwqCHe8J9yWM0Rdf0EPJCvaX-N3mX7SJ8VOcK3oDdminUXiPRDQxW3t7p8vfJXdq6g7HHqcm7CVFeDp7rT6OWPpWCG8lGnE2FOLwBtuEdl5uuH2K9ubaJ_qEdLMD5Xl6HrTYr2U9kboBLPtwLnIO4X3CGEnYAAVtOJ0RlHhcoYbmhK5zioGDUls_jI6c_50pba36gkKXkGsUJOUEANLE-kshE8yQbMGLYVVI_4snHhZRlTkkRPLjHFxxiHfhEOqh3nV1WMgQwVy3NaRxdWoiLY6Uccjrl1UZE3wVm7kFMT1R-OPlHF0i--wuof9UEBh4rFXLCqkA6Yvl9WVg1XxAm_gXiJj51NVQAooz7SScEmenSQRHZOdYpTk_IYYvobPPBfOjD7YOD9mFPfjobj6voN9vWAHobz7G1nXMlxmjc8udKbotuau9pwgGoSsqn75Iu6w778ZUprwE_oRg-5Sv3PkLym21-jrVy6UU5ofVzdZBDMCbEfYnvGZfiJSyCxFSybTpV7CHErUuAwd9ppyHlOPxKqvXosX-nEQ4Rwp7ctW8mXK0aka0cqGffYd0a8LTkXI4u41Zcr8f3TpM4tAd13KVf6jhApqsaDE2Mu6NGcsjWZZkrQ6bd4QlvFY4zYnE_OFsmsA=w221-h220-no?authuser=0';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
